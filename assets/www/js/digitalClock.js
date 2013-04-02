08f2d86b6b33b9d2a2ba7970ee9994f523695602
TiffanyChou <ticho0823@gmail.com>
2013-04-02 19:30:31 -0400

    initial commit

---
+++ assets/www/js/digitalClock.js
@@ -0,0 +1,15 @@
+setInterval(function(){
+	var currentTime = new Date();
+	var h ="0"+currentTime.getHours();
+	var m ="0"+currentTime.getMinutes();
+	var s ="0"+currentTime.getSeconds();
+	
+	h = h.substr(h.length-2, 2);
+	m = m.substr(m.length-2, 2);
+	s = s.substr(s.length-2, 2);
+	
+	var timestr =h+":"+m+":"+s;
+	document.getElementById("clock").innerHTML = timestr;},1000);
+
+
+setTimeout("window.scrollTo(0, 1)", 10);
